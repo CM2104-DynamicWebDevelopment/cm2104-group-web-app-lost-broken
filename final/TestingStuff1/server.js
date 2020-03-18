07357197f25c178e1ea4774ce551af9f56693416
AshAudio <ashleighsinclair86@gmail.com>
2020-03-18 15:30:08 +0000

    bugs

--- final/TestingStuff1/server.js
+++ final/TestingStuff1/server.js
@@ -9,7 +9,8 @@ app.get('/doAPICall', function(req, res){
     var returnJSON = [];
     var url = "https://freesound.org/apiv2/search/text/?query=" + req.query + "&page_size=1&fields=name&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ"; 
     console.log(req.query);
-    console.log(url);
+    console.log(req.params);
+    console.log(req);
     request(url, { json: true }, (err, res, init) => {
         if (err) { return console.log(err); }
         if(init.count > 0){ //if at least one item in response
