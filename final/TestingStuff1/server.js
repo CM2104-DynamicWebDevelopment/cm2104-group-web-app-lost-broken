f9aa3eb3e5a19a113544f5f93bc90f37abaa9fac
AshAudio <ashleighsinclair86@gmail.com>
2020-03-18 13:43:04 +0000

    receiving json

--- final/TestingStuff1/server.js
+++ final/TestingStuff1/server.js
@@ -4,11 +4,8 @@ const app = express();
 
 app.use(express.static('public'))
 
-
-
-
 app.get('/doAPICall', function(req, res){
-    var url = "https://freesound.org/apiv2/search/text/?query=" + req.searchTerms + "&page_size=1&fields=name&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ"; 
+    var url = "https://freesound.org/apiv2/search/text/?query=" + req.query + "&page_size=1&fields=name&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ"; 
     https.get(url, (resp) => {
     let data = '';
 
@@ -20,6 +17,7 @@ app.get('/doAPICall', function(req, res){
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
         console.log(JSON.parse(data).explanation);
+        res.send(data);
         /*var json = [];
         for (var i = 0; i < tweets.length; i++) {
 		json.push({
@@ -27,7 +25,7 @@ app.get('/doAPICall', function(req, res){
 			text: tweets[i].text
 		});
         }
-        res.send(output);*/
+        */
     });
 
     }).on("error", (err) => {
