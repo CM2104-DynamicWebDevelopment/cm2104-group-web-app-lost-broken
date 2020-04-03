3efcc7659f9bccc00e9fdd244119a9201d23dcb6
AshAudio <ashleighsinclair86@gmail.com>
2020-04-03 12:52:28 +0100

    Removed Index, consolidate Controls.js

--- final/beta2/public/scripts/Controls.js
+++ final/beta2/public/scripts/Controls.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
 	$(".searchicon").hover(function(){ //get the searchicon element on mouseover, and change the icon
 		$(".searchicon").css({"background": "url('resources/speakerIcon.svg')", "background-size" : "32px 32px"});
 		});
@@ -6,7 +6,29 @@ $(document).ready(function(){
 	$(".searchicon").mouseleave(function(){  //get the searchicon element on mouse leave, and change the icon
 		$(".searchicon").css({"background": "url('resources/speakerIconNoAudio.svg')", "background-size" : "32px 32px"});
 	});
+	$('.searchicon').on('click', function () {
+		var params = {search: $('.searchtext').val()};
+        getResults(params);
+        return false;
+    });
  });
+
+function getResults(params){
+	$.getJSON("/doAPICall", params)
+    .done(function(response) {
+		var duration = response[0].duration;
+		var previewURL = response[0].previewURL;
+		var waveformURL = response[0].waveformURL;
+		var title = response[0].title;
+		var description = response[0].description;
+		var username = response[0].username;
+		createAudioElement(previewURL);
+        createDetailsDiv(title, username, description, duration, waveformURL);
+    })
+    .fail(function(){
+        console.log("Invalid API response");
+    });
+}
 
 function playAudio() { 
 	$("#audioElement")[0].play();
