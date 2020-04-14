01a3de78be5c48c4f3fe473dcb60f7e562421082
AshAudio <ashleighsinclair86@gmail.com>
2020-04-14 10:53:49 +0100

    changed JSON object from array to JSON

--- final/beta3/server.js
+++ final/beta3/server.js
@@ -22,7 +22,14 @@ app.get('/about', function(req, res) {
 });
 
 app.get('/doAPICall', function(req, res){
-    var returnJSON = {success: "false" };
+    var returnJSON = {success: "false",
+                        title: "",
+                        duration: 0,
+                        previewURL: "",
+                        waveformURL: "",
+                        description: "",
+                        username: "",
+                        url: ""};
     var getLengthURL = "https://freesound.org/apiv2/search/text/?query=" + req.query.search + "&page_size=1&filter=duration:[0.2 TO 20]&fields=name&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ"; 
     request(getLengthURL, { json: true }, (err, resp, init) => {
         if (err) { return console.log(err); }
@@ -34,7 +41,14 @@ app.get('/doAPICall', function(req, res){
                 if (err) { return console.log(err); }
                 var rand = Math.floor(Math.random() * (full.results.length-1));
                 returnJSON["success"] = "true";
-                returnJSON.push({ //fill returnJSON with just one item
+                returnJSON["title"] = full.results[rand].name;
+                returnJSON["duration"] = full.results[rand].duration;
+                returnJSON["previewURL"] = full.results[rand].previews["preview-lq-ogg"];
+                returnJSON["waveformURL"] = full.results[rand].images.waveform_l;
+                returnJSON["description"] = full.results[rand].description;
+                returnJSON["username"] = full.results[rand].username;
+                returnJSON["url"] = full.results[rand].url;
+                /*returnJSON.push({ //fill returnJSON with just one item
                     title: full.results[rand].name,
                     duration: full.results[rand].duration,
                     previewURL: full.results[rand].previews["preview-lq-ogg"],
@@ -42,7 +56,7 @@ app.get('/doAPICall', function(req, res){
                     description: full.results[rand].description,
                     username: full.results[rand].username,
                     url: full.results[rand].url
-                });
+                });*/
                 console.log("in Func" + returnJSON);
                 //res.send(returnJSON);
             });
