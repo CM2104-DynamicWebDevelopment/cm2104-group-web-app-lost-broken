98f2eae5c681224025a5d32699d4b5764365947b
AshAudio <ashleighsinclair86@gmail.com>
2020-04-14 10:15:53 +0100

    limit duration API call

--- final/beta3/server.js
+++ final/beta3/server.js
@@ -23,13 +23,13 @@ app.get('/about', function(req, res) {
 
 app.get('/doAPICall', function(req, res){
     var returnJSON = [];
-    var getLengthURL = "https://freesound.org/apiv2/search/text/?query=" + req.query.search + "&page_size=1&fields=name&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ"; 
+    var getLengthURL = "https://freesound.org/apiv2/search/text/?query=" + req.query.search + "&page_size=1&filter=duration:[0.2 TO 30]&fields=name&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ"; 
     request(getLengthURL, { json: true }, (err, resp, init) => {
         if (err) { return console.log(err); }
         if(init.count > 0){ //if at least one item in response
             var searchPage = Math.floor(Math.random() * (init.count/15)); //pick a random page using the response (init.count is total items, 15 is the page length)
             //next line constructs the main API request
-            var getDataURL = "https://freesound.org/apiv2/search/text/?query=" + req.query.search + "&page=" + searchPage.toString() + "&page_size=15&fields=name,description,previews,duration,username,images,url&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ";
+            var getDataURL = "https://freesound.org/apiv2/search/text/?query=" + req.query.search + "&page=" + searchPage.toString() + "&page_size=15&filter=duration:[0.2 TO 30]&fields=name,description,previews,duration,username,images,url&token=qxCIuynZMi8Cmvw70H1aPMKofG87c6LFuZ2PvbSZ";
             request(getDataURL, { json: true }, (err, resp, full) => {
                 if (err) { return console.log(err); }
                 var rand = Math.floor(Math.random() * (full.results.length-1));
