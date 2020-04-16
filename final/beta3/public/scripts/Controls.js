6fc65bbf287c23f28b74883a6b6a82ddc058a678
AshAudio <ashleighsinclair86@gmail.com>
2020-04-16 17:15:51 +0100

    404

--- final/beta3/public/scripts/Controls.js
+++ final/beta3/public/scripts/Controls.js
@@ -64,7 +64,7 @@ function favAudio() {
 	//document.getElementById("favForm").submit(); //form submission
 	
 
-	$.post('favsound', {title:sessionStorage.getItem("title"), image:sessionStorage.getItem("image"),  sound:sessionStorage.getItem("sound")}, function(response){ 
+	$.post("/favsound", {title:sessionStorage.getItem("title"), image:sessionStorage.getItem("image"),  sound:sessionStorage.getItem("sound")}, function(response){ 
 		console.log(response);
 		alert("success");
 	});
