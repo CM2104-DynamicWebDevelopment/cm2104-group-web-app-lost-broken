5b3f5db47a3bfb2735acf1fa96afc235076e10b5
AshAudio <ashleighsinclair86@gmail.com>
2020-04-23 21:37:00 +0100

    fav method now accepts params, can be used to delete profile entries

--- final/beta3/public/scripts/Controls.js
+++ final/beta3/public/scripts/Controls.js
@@ -14,7 +14,7 @@ $(function(){
 		}
 		catch(err){
 			console.log(err);
-			//add message to tell user it failed...
+			showFailureMessage();
 		}
 		finally{
 			return false;
@@ -43,7 +43,6 @@ function getResults(params){
     })
     .fail(function(){
 		console.log("Invalid API response");
-		//add message to tell user it failed? maybe should have a timeout
 		showFailureMessage();
     });
 }
@@ -59,7 +58,43 @@ function stopAudio() {
 	a.currentTime = 0.0;
 }
 
+//when fav button on index page is clicked, pass parameters to addRemoveFav
 function favAudio() { 
+	addRemoveFav(sessionStorage.getItem("title"), sessionStorage.getItem("image"), sessionStorage.getItem("sound"), $(".favIco"), false);
+}
+
+//contacts server and awaits response, then either toggles fav button colour or deletes entry if deleteContainer is set to true
+function addRemoveFav(title, image, sound, buttonPointer, deleteContainer) { 
+	//var containing sound data
+	var datatosend = {
+		"title":title,
+		"image":image,
+		"sound":sound};
+	//AJAX POST to favsound route
+	$.ajax({
+		type:"POST",
+		data:datatosend,
+		url:"favsound",
+		success: function(response)
+		{
+			if(response.redirect == "true")
+				window.location = "/login";
+			if(response.save == "true")
+				buttonPointer.css({"background-color": "yellow"});
+			else if(!deleteContainer) //if not deleteContainer then toggle colour
+				buttonPointer.css({"background-color": "transparent"});
+			else //if deleteContainer then pass element to deleteEntry(elementToDelete)
+				deleteEntry(buttonPointer.parent());
+		}
+	});
+}
+
+function deleteEntry(elementToDelete){
+	elementToDelete.remove();
+}
+
+// Backup of original favAudio()
+/*function favAudio() { 
 	var datatosend = {
 		"title":sessionStorage.getItem("title"),
 		"image":sessionStorage.getItem("image"),
@@ -79,4 +114,4 @@ function favAudio() {
 				$(".favIco").css({"background-color": "transparent"});
 		}
 	});
-}
+}*/
