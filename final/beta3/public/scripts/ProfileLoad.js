3f21ccab380f7a2397023664659d7d07cea0c17b
AshAudio <ashleighsinclair86@gmail.com>
2020-04-28 16:37:09 +0100

    more cleaning and linked buildFavList to check

--- final/beta3/public/scripts/ProfileLoad.js
+++ final/beta3/public/scripts/ProfileLoad.js
@@ -3,27 +3,27 @@ $(function() { //runs once page is loaded
 });
 
 function getSounds() {
-    $.post("doUserSearch").done(function( response ) {
+    $.post("doUserSearch").done(function(response) {
         console.log(response);
         console.log(response[0].title);
         console.log(response[1].title);
+        console.log(response[1].image);
         console.log(response[2].sound);
         //this gets the response from the server, then you can loop through array etc and pass to 
-        //buildFavList(response); however you want
+        buildFavList(response);
       });
 }
 
 function buildFavList(response){
-    var favDiv = $('#favs');
     var faveList = [];
-    for (var i = 0; i < title.length; i++) {
-        faveList.push({title: title[i], user: user[i]});
+    for (var i = 0; i < response.length; i++) {
+        faveList.push({title: response[i].title, sound: response[i].sound});
     }
     faveList.forEach(populateList)
     //construct html of each item and append
     function populateList(item){
-    var itemHTML = '<li class="list"><div id="listTitle"><h3>' + item.title + '</h3></div>';
-    itemHTML += '<div id="listUser"><h3>' + item.user + '</h3></div></li>';
-    favDiv.append(itemHTML);
+        var itemHTML = '<li class="list"><div id="listTitle"><h3>' + item.title + '</h3></div>';
+        itemHTML += '<div id="listUser"><h3>' + item.sound + '</h3></div></li>';
+        $('#favs').append(itemHTML);
     }
 }
