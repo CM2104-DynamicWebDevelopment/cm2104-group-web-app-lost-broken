3dbe0b7b0338e71fa1ab19a81274d2b13d7a2bcd
wesmerrett <1803561@rgu.ac.uk>
2020-04-28 15:15:54 +0100

    eh?

--- final/beta3/server.js
+++ final/beta3/server.js
@@ -230,7 +230,7 @@ app.post('/sign_up', function(req, res){
   var passw_c = req.body.pass;
   //validates that entered data is correct
   //checks if all boxes have been filled in
-  if (!passw_c || !usern || !passw){
+  if ((!passw_c) || (!usern) || (!passw)){
     res.redirect('/register');
     console.log("details missing");
   }
