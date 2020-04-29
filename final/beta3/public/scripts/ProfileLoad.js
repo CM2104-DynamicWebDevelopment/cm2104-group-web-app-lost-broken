a96285501520c6e0bdb7ff530f8b9fc2b012b48d
Scorpio11Dragon <1802258@rgu.ac.uk>
2020-04-29 20:26:25 +0100

    d

--- final/beta3/public/scripts/ProfileLoad.js
+++ final/beta3/public/scripts/ProfileLoad.js
@@ -14,9 +14,9 @@ function buildFavList(response){
         //alaric things will need linked to play and stop buttons here probably? im not sure how its planned but
         //this still will need to be played around with however you want do it
         var itemHTML = '<li class="list"><div id="listTitle"><h3>' + response[i].title + '</h3></div>';
-        itemHTML += '<div id="listSoundPlay"><button class="controlButtons playIco" onclick="playAudio(response[i].sound)" alt="Play"><i class="fas fa-play"></i></button>'
-        itemHTML += '<div id="listSoundStop"><button class="controlButtons stopIco" onclick="stopAudio(response[i].sound)" alt="Stop"><i class="fas fa-stop"></i></button>'
-        itemHTML += '<button class="controlButtons favIco" onclick="favAudio()" alt="Fav"><i class="fas fa-star"></i></button>'
+        //itemHTML += '<div id="listSoundPlay"><button class="controlButtons playIco" onclick="playAudio(response[i].sound)" alt="Play"><i class="fas fa-play"></i></button>'
+        //itemHTML += '<div id="listSoundStop"><button class="controlButtons stopIco" onclick="stopAudio(response[i].sound)" alt="Stop"><i class="fas fa-stop"></i></button>'
+        //itemHTML += '<button class="controlButtons favIco" onclick="favAudio()" alt="Fav"><i class="fas fa-star"></i></button>'
         itemHTML += '<div id="listUser"><h3>' + response[i].user + '</h3></div></li>';
         $('#favs').append(itemHTML);
     }
