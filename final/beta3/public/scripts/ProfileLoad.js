7b023b33dce6c8e157631d65b680cdc0cbb67b7f
AshAudio <ashleighsinclair86@gmail.com>
2020-04-30 16:20:27 +0100

    test

--- final/beta3/public/scripts/ProfileLoad.js
+++ final/beta3/public/scripts/ProfileLoad.js
@@ -37,7 +37,8 @@ function buildFavList(response){
 
 //unfaves the audio, passes elementToDelete
 function unfavAudio(i){
-	var title = $("#favs .list").find("#listTitle")[i].text();
+	//var title = $("#favs .list").find("#listTitle")[i].text();
+	var title = $("#favs .list").find("#listTitle")[i];
 	console.log(title);
 	addRemoveFav(sessionStorage.getItem("title")[i], sessionStorage.getItem("image")[i], sessionStorage.getItem("sound")[i], $("#favs .list")[i], true);
 	//deleteIndex(i);
