ad533373f395a8a9ada7556ee7fbe725ddc45c8a
Scorpio11Dragon <1802258@rgu.ac.uk>
2020-04-30 02:46:31 +0100

    j

--- final/beta3/public/scripts/ProfileLoad.js
+++ final/beta3/public/scripts/ProfileLoad.js
@@ -14,11 +14,22 @@ function buildFavList(response){
         //alaric things will need linked to play and stop buttons here probably? im not sure how its planned but
         //this still will need to be played around with however you want do it
         var itemHTML = '<li class="list"><div id="listTitle"><h3>' + response[i].title + '</h3></div>';
-        itemHTML += '<button class="controlButtonsP playIco" onclick="playAudio(response[i].sound)" alt="Play"><i class="fas fa-play"></i></button></div>'
-        itemHTML += '<button class="controlButtonsP stopIco" onclick="stopAudio(response[i].sound)" alt="Stop"><i class="fas fa-stop"></i></button></div>'
+        itemHTML += '<button class="controlButtonsP playIco" onclick="playSavAudio(response[i].sound)" alt="Play"><i class="fas fa-play"></i></button></div>'
+        itemHTML += '<button class="controlButtonsP stopIco" onclick="stopSavAudio(response[i].sound)" alt="Stop"><i class="fas fa-stop"></i></button></div>'
         itemHTML += '<button class="controlButtonsP favIco" onclick="favAudio()" alt="Fav"><i class="fas fa-star"></i></button></div>'
         itemHTML += '<div id="listUser"><h3>' + response[i].user + '</h3></div></li>';
         $('#favs').append(itemHTML);
     }
+}
+
+function playSavAudio() { 
+	$(response[i].sound)[0].play();
+}
+
+function stopSavAudio() { 
+	var a = document.getElementById(response[i.sound]);
+	//there is no stop function in html5? so pause it and reset time to start
+	a.pause(); 
+	a.currentTime = 0.0;
 }
 
